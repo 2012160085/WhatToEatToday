801cb21a6cc68b3caaea3bd01357ac6858ebc56d
kibeom lee <70303094+2012160085@users.noreply.github.com>
2021-03-08 03:41:56 +0900

    Update WhattoeatApplication.java

--- src/main/java/com/pixter/whattoeat/WhattoeatApplication.java
+++ src/main/java/com/pixter/whattoeat/WhattoeatApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WhattoeatApplication {
 
 	public static void main(String[] args) {
+		//test1
 		SpringApplication.run(WhattoeatApplication.class, args);
 	}
 
